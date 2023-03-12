d80b8aaaae9079862b330094f9c4f8de6512b722
jasonhall0081 <jason.hall0081@gmail.com>
2023-03-12 15:38:30 +0100

    Default annotation explained

--- server/common/src/main/java/cenglisch/Default.java
+++ server/common/src/main/java/cenglisch/Default.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines the default used constructor.
+ * This annotation is required for Mapstruct, its declares where values injected.
+ */
 @Target(ElementType.CONSTRUCTOR)
 @Retention(RetentionPolicy.CLASS)
 public @interface Default {
